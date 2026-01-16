4496a04228f648f87ed750afcd7dfd020470ecfe
Davyd Krykota <krikotadavid957@gmail.com>
2026-01-16 20:50:24 +0200

    feat: create products module with functionality of getting and creating products

---
+++ src/products/products.controller.ts
@@ -0,0 +1,18 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { ProductsService } from './products.service';
+import { ProductDto } from '../dtos/product.dto';
+import { CreateProductDto } from '../dtos/createProduct.dto';
+import { Prisma } from '@prisma/client';
+@Controller('products')
+export class ProductsController {
+  constructor(private readonly productService: ProductsService) {}
+
+  @Get()
+  getProducts(): Promise<ProductDto[]> {
+    return this.productService.getProducts();
+  }
+  @Post()
+  createProduct(@Body() dto: CreateProductDto) {
+    return this.productService.createProduct(dto);
+  }
+}
---
+++ src/products/products.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { ProductDto } from '../dtos/product.dto';
+import { CreateProductDto } from '../dtos/createProduct.dto';
+@Injectable()
+export class ProductsService {
+  constructor(private prisma: PrismaService) {}
+  async getProducts(): Promise<ProductDto[]> {
+    const products = await this.prisma.product.findMany({
+      select: {
+        productCode: true,
+        name: true,
+        price: true,
+      },
+    });
+    return products.map((product) => ({
+      productCode: product.productCode,
+      name: product.name,
+      price: product.price.toNumber(),
+    }));
+  }
+  async createProduct(data: CreateProductDto) {
+    const createdProduct = await this.prisma.product.create({ data });
+    return {
+      productCode: createdProduct.productCode,
+      name: createdProduct.name,
+      price: createdProduct.price.toNumber(),
+    };
+  }
+}
