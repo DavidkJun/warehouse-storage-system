e3bb673a8f6845cf50ba0f40c858881d4a3b256d
Davyd Krykota <krikotadavid957@gmail.com>
2026-01-16 20:54:56 +0200

    prisma and warehouses initiated

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { ProductsModule } from './products/products.module';
+import { WarehousesModule } from './warehouses/warehouses.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, ProductsModule, WarehousesModule],
   controllers: [AppController],
   providers: [AppService],
 })
