70eeace4548faf349c237cdd30fe38acebac47da
Davyd Krykota <krikotadavid957@gmail.com>
2026-01-20 18:54:54 +0200

    feat: implemented warehouse creating

--- src/warehouses/warehouses.controller.ts
+++ src/warehouses/warehouses.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { WarehousesService } from './warehouses.service';
+import { CreateWarehouseDto } from '../dtos/createWarehouse.dto';
 
 @Controller('warehouses')
-export class WarehousesController {}
+export class WarehousesController {
+  constructor(private readonly warehouseService: WarehousesService) {}
+  @Get()
+  getWarehouses() {
+    return this.warehouseService.getWarehouses();
+  }
+  @Post()
+  createWarehouse(@Body() dto: CreateWarehouseDto) {
+    return this.warehouseService.createWarehouse(dto);
+  }
+}
--- src/warehouses/warehouses.service.ts
+++ src/warehouses/warehouses.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { WarehouseDto } from '../dtos/warehouse.dto';
+import { CreateWarehouseDto } from '../dtos/createWarehouse.dto';
 
 @Injectable()
-export class WarehousesService {}
+export class WarehousesService {
+  constructor(private prisma: PrismaService) {}
+  getWarehouses(): Promise<WarehouseDto[]> {
+    return this.prisma.warehouse.findMany();
+  }
+  async createWarehouse(data: CreateWarehouseDto) {
+    const createdWarehouse = await this.prisma.warehouse.create({ data });
+    return {
+      name: createdWarehouse.name,
+      address: createdWarehouse.address,
+    };
+  }
+}
