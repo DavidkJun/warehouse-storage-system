b136286c246040b2b0c3fa8f30a3a6094d660f28
Davyd Krykota <krikotadavid957@gmail.com>
2026-02-19 18:55:07 +0200

    feat: add data validation

--- src/dtos/createInventory.dto.ts
+++ src/dtos/createInventory.dto.ts
@@ -1,5 +1,15 @@
+import { IsInt, IsPositive, IsNumber } from 'class-validator';
+import { Type } from 'class-transformer';
+
 export class CreateInventoryDto {
+  @IsInt()
   productId: number;
+
+  @IsInt()
+  @IsPositive()
+  @Type(() => Number)
   quantity: number;
+
+  @IsInt()
   warehouseId: number;
 }
