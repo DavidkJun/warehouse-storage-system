11db706c90a7c18ce131e5516d7bd3660238ceff
Davyd Krykota <krikotadavid957@gmail.com>
2026-02-27 21:26:27 +0200

    feat: protected inventory routes so only admin can modify them

--- src/inventories/inventories.controller.ts
+++ src/inventories/inventories.controller.ts
@@ -6,13 +6,18 @@ import {
   Delete,
   ParseIntPipe,
   Param,
+  UseGuards,
 } from '@nestjs/common';
 import { InventoriesService } from './inventories.service';
 import { CreateInventoryDto } from '../dtos/createInventory.dto';
 import { Inventory } from '@prisma/client';
 import { TransferInventoryDto } from '../dtos/transferInventory.dto';
+import { JwtGuard } from 'src/Guards/jwt.guard';
+import { RolesGuard } from 'src/Guards/roles.guard';
+import { Roles } from 'src/Decorators/roles.decorator';
 
 @Controller('inventories')
+@UseGuards(JwtGuard, RolesGuard)
 export class InventoriesController {
   constructor(private readonly inventoriesService: InventoriesService) {}
 
@@ -21,11 +26,13 @@ export class InventoriesController {
     return this.inventoriesService.getInventories();
   }
 
+  @Roles('ADMIN')
   @Post()
   async insertInventory(@Body() dto: CreateInventoryDto): Promise<Inventory> {
     return await this.inventoriesService.insertInventory(dto);
   }
 
+  @Roles('ADMIN')
   @Delete(':id')
   async deleteInventory(
     @Param('id', ParseIntPipe) id: number
@@ -33,6 +40,7 @@ export class InventoriesController {
     return await this.inventoriesService.deleteInventory(id);
   }
 
+  @Roles('ADMIN')
   @Post('transfer')
   async transferInventory(@Body() dto: TransferInventoryDto) {
     return await this.inventoriesService.transferInventory(dto);
